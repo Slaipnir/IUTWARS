ea8f051c61890b09f6087cb67f0d66d79e341a2c
ACayet <arthurcayet@hotmail.fr>
2018-02-20 10:00:47 +0100

    FIN

--- Assets/Scripts/REMPLISS.cs
+++ Assets/Scripts/REMPLISS.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class REMPLI : MonoBehaviour {
+public class REMPLISS : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Textures and Sprites/REMPLIS.cs
+++ Assets/Textures and Sprites/REMPLIS.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class REMPLI : MonoBehaviour {
+public class REMPLIS : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
